acf8310901de3eb93f148cd1e6aced5bdeceb81d
TeeJude <teejayjude@gmail.com>
2024-01-15 20:00:11 +0000

    4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -5,9 +5,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Take the code from wait_n and alter it into a new function task_wait_n.
-       The code is nearly identical to wait_n except task_wait_random is being
-       called."""
+    """Take the code from wait_n and alter it into a new function task_wait_n."""
     delays: List[float] = []
     all_delays: List[float] = []
     for i in range(n):
